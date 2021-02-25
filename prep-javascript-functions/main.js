ac87d4ee37c6c0385f171bba292eb43884ac3b26
Johnathan Nguyen <haungocvo@gmail.com>
2021-02-24 22:10:16 -0800

    Finished Add and multiply by 5

--- prep-javascript-functions/main.js
+++ prep-javascript-functions/main.js
@@ -14,3 +14,8 @@ function personalizedGreeting(name) {
 }
 const personalizedGreetingResult = personalizedGreeting('World');
 console.log(personalizedGreetingResult);
+function addAndMultiplyBy5(num1, num2) {
+  return (num1 + num2) * 5;
+}
+const addAndMultiplyBy5Result = addAndMultiplyBy5(10, 5);
+console.log(addAndMultiplyBy5Result);
