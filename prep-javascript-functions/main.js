bfb0ef29b21db97768b0bc3cde1c9765e673f98a
Johnathan Nguyen <haungocvo@gmail.com>
2021-02-24 21:54:23 -0800

    Finished convert hour to min func

---
+++ prep-javascript-functions/main.js
@@ -0,0 +1,10 @@
+function addTwoNumbers(num1, num2) {
+  return num1 + num2;
+}
+console.log(addTwoNumbers(4, 4));
+
+function convertHoursToMinutes(number) {
+  return number * 60;
+}
+const convertHoursToMinutesResult = convertHoursToMinutes(2);
+console.log(convertHoursToMinutesResult);
